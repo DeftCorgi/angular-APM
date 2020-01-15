cc224fa09ff98e0968516d2c2c92a1aea58b9e34
Toan Au <Toan.Au@telstrasuper.com.au>
2020-01-16 09:37:09 +1100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ProductListComponent],
+  bootstrap: [AppComponent, ProductListComponent]
+})
+export class AppModule { }
